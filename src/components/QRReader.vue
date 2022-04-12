d1f7da79bb5c4ce38fd2a91674e337782a1d5239
Alexandr <87748202+shadowHD-bit@users.noreply.github.com>
2022-04-12 14:14:31 +0700

    added new remarks

--- src/components/QRReader.vue
+++ src/components/QRReader.vue
@@ -1,10 +1,12 @@
 
 <template>
         <div class="info_containe">
-            <p>{{ error }}</p>
-            <p>{{ decodedString }}</p>
+            <p class="info">Please point the QR code at the camera and you will get the result:</p>
+            <p class="error">{{ error }}</p>
+            <p class="success">{{ decodedString }}</p>
         </div>
         <div class="camera_contai">
+            <img src="@/assets/camera.svg"/>
             <qrcode-stream @init="onInit" @decode="onDecode"></qrcode-stream>
         </div>
 </template>
@@ -31,17 +33,17 @@ export default {
       // successfully initialized
         } catch (error) {
         if (error.name === 'NotAllowedError') {
-            this.error = "user denied camera access permisson"
+            this.error = "ERROR: User denied camera access permisson"
         } else if (error.name === 'NotFoundError') {
-            this.error = "no suitable camera device installed"
+            this.error = "ERROR: No suitable camera device installed"
         } else if (error.name === 'NotSupportedError') {
-            this.error = "page is not served over HTTPS (or localhost)"
+            this.error = "ERROR: Page is not served over HTTPS (or localhost)"
         } else if (error.name === 'NotReadableError') {
-            this.error = "maybe camera is already in use"
+            this.error = "ERROR: Maybe camera is already in use"
         } else if (error.name === 'OverconstrainedError') {
-            this.error = "did you requested the front camera although there is none?"
+            this.error = "ERROR: Did you requested the front camera although there is none?"
         } else if (error.name === 'StreamApiNotSupportedError') {
-            this.error = "browser seems to be lacking features"
+            this.error = "ERROR: Browser seems to be lacking features"
         }
         } finally {
         // hide loading indicator
@@ -64,6 +66,7 @@ export default {
         width: 50%;
         height: 100%;
         display: flex;
+        flex-direction: column;
         justify-content: center;
         align-items: center;
     }
@@ -73,6 +76,28 @@ export default {
         display: flex;
         justify-content: center;
         align-items: center;
+        border: 1px solid rgb(20, 162, 206);
+        border-radius: 30px;
+    }
+
+    .error{
+        color: red;
+        font-size: 20px;
+
+    }
+
+    .success{
+        color: green;
+        font-size: 20px;
+    }
+
+    .info{
+        font-size: 20px;
+    }
+
+    img{
+        width: 15%;
+        position: absolute;
     }
 
 
